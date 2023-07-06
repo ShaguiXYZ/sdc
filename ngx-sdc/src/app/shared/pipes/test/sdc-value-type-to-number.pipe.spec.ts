df45c0e50a0fef0a87cde6cc7d7810106d3a8eb6
DABAN MACIA Ernest <ernest.daban@soprasteria.com>
2023-07-06 13:43:58 +0200

    NGX Tests

--- ngx-sdc/src/app/shared/pipes/test/sdc-value-type-to-number.pipe.spec.ts
+++ ngx-sdc/src/app/shared/pipes/test/sdc-value-type-to-number.pipe.spec.ts
@@ -15,4 +15,12 @@ describe('SdcValueTypeToNumberPipe', () => {
   it('should transform a string to a version', () => {
     expect(pipe.transform('0.0.1', ValueType.VERSION)).toBe(0.01);
   });
+
+  it('should transform a string to a version case one', () => {
+    expect(pipe.transform('false', ValueType.BOOLEAN)).toBe(0);
+  });
+
+  it('should transform a string to a version case two', () => {
+    expect(pipe.transform('true', ValueType.BOOLEAN)).toBe(1);
+  });
 });
