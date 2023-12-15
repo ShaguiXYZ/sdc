d6eecc14297e2c4f1b22517ad94f139b9edffd6c
Shagui <shaguixyz@gmail.com>
2023-12-15 14:37:12 +0100

    trend background

--- ngx-sdc/src/app/shared/components/sdc-trend/sdc-trend.component.ts
+++ ngx-sdc/src/app/shared/components/sdc-trend/sdc-trend.component.ts
@@ -5,11 +5,12 @@ import { Component, Input } from '@angular/core';
   selector: 'sdc-trend',
   styleUrl: './sdc-trend.component.scss',
   template: `
+    <div class="coverage-trend-background color--{{ trend > 0 ? 'acceptable' : trend < 0 ? 'critical' : 'with_risk' }}"></div>
     <div
       class="coverage-trend sdc-center color--{{ trend > 0 ? 'acceptable' : trend < 0 ? 'critical' : 'with_risk' }}"
       [attr.data-title]="trend"
     >
-      <em class="sdc-center fa-solid fa-arrow-{{ trend > 0 ? 'up' : trend < 0 ? 'down' : 'right' }}" aria-hidden="true"></em>
+      <em class="sdc-center fa-regular fa-{{ trend > 0 ? 'face-smile' : trend < 0 ? 'face-frown' : 'face-meh' }}" aria-hidden="true"></em>
     </div>
   `,
   standalone: true,
