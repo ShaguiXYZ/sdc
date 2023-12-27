c4deab4f1f9041c9e96e4faa53cc563348dd2073
Shagui <shaguixyz@gmail.com>
2023-12-27 11:30:16 +0100

    bff sse controller request params

--- bff-sdc/src/main/java/com/shagui/sdc/api/SseEventApi.java
+++ bff-sdc/src/main/java/com/shagui/sdc/api/SseEventApi.java
@@ -1,11 +1,15 @@
 package com.shagui.sdc.api;
 
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.shagui.sdc.api.dto.sse.EventDTO;
+import com.shagui.sdc.api.dto.sse.EventType;
 
 import feign.Headers;
 import reactor.core.publisher.Flux;
@@ -14,5 +18,5 @@ import reactor.core.publisher.Flux;
 @RequestMapping(path = { "/api" }, produces = { MediaType.TEXT_EVENT_STREAM_VALUE })
 public interface SseEventApi {
     @GetMapping(value = "/events/{workflowId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    Flux<EventDTO> getEvents(@PathVariable String workflowId);
+    Flux<EventDTO> getEvents(@PathVariable String workflowId, @RequestParam(required = false) List<EventType> types);
 }
--- bff-sdc/src/main/java/com/shagui/sdc/controller/SseEventController.java
+++ bff-sdc/src/main/java/com/shagui/sdc/controller/SseEventController.java
@@ -1,9 +1,12 @@
 package com.shagui.sdc.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shagui.sdc.api.SseEventApi;
 import com.shagui.sdc.api.dto.sse.EventDTO;
+import com.shagui.sdc.api.dto.sse.EventType;
 import com.shagui.sdc.service.SseService;
 
 import lombok.AllArgsConstructor;
@@ -15,7 +18,8 @@ public class SseEventController implements SseEventApi {
     private final SseService sseService;
 
     @Override
-    public Flux<EventDTO> getEvents(String workflowId) {
-        return this.sseService.asFlux().filter(event -> event.getWorkflowId().equals(workflowId));
+    public Flux<EventDTO> getEvents(String workflowId, List<EventType> types) {
+        return this.sseService.asFlux().filter(event -> event.getWorkflowId().equals(workflowId))
+                .filter(null == types ? event -> true : event -> types.contains(event.getType()));
     }
 }
