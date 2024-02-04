560db39a3a88a9d95c53c28ef68801890c9407ee
Shagui <shaguixyz@gmail.com>
2024-02-05 00:59:56 +0100

    size chart model refactor

--- ngx-sdc/src/app/shared/components/sdc-charts/models/size.model.ts
+++ ngx-sdc/src/app/shared/components/sdc-charts/models/size.model.ts
@@ -3,7 +3,14 @@ import { DataInfo } from 'src/app/core/models';
 export type ChartSize =
   | number
   | string
-  | { height?: number | string; width?: number | string; 'max-height'?: number | string; 'max-width'?: number | string };
+  | {
+      height?: number | string;
+      width?: number | string;
+      'max-height'?: number | string;
+      'max-width'?: number | string;
+      'min-height'?: number | string;
+      'min-width'?: number | string;
+    };
 
 export class SdcChartSize {
   constructor(private size?: ChartSize) {}
@@ -12,39 +19,52 @@ export class SdcChartSize {
     const styleSize: DataInfo<number | string> = {};
 
     if (typeof this.size === 'number') {
-      styleSize['height.px'] = this.size;
-      styleSize['width.px'] = this.size;
+      this.setNumericStyleSize(styleSize, this.size);
     } else if (typeof this.size === 'string') {
-      styleSize['height'] = this.size;
-      styleSize['width'] = this.size;
+      this.setStringStyleSize(styleSize, this.size);
     } else if (typeof this.size === 'object') {
-      const { height, width, 'max-height': maxHeight, 'max-width': maxWidth } = this.size;
-
-      if (typeof height === 'string') {
-        styleSize['height'] = height;
-      } else if (typeof height === 'number') {
-        styleSize['height.px'] = height;
-      }
-
-      if (typeof width === 'string') {
-        styleSize['width'] = width;
-      } else if (typeof width === 'number') {
-        styleSize['width.px'] = width;
-      }
-
-      if (typeof maxHeight === 'string') {
-        styleSize['max-height'] = maxHeight;
-      } else if (typeof maxHeight === 'number') {
-        styleSize['max-height.px'] = maxHeight;
-      }
-
-      if (typeof maxWidth === 'string') {
-        styleSize['max-width'] = maxWidth;
-      } else if (typeof maxWidth === 'number') {
-        styleSize['max-width.px'] = maxWidth;
-      }
+      this.setObjectStyleSize(styleSize, this.size);
     }
 
     return styleSize;
+  }
+
+  private setNumericStyleSize(styleSize: DataInfo<number | string>, size: number): void {
+    styleSize['height.px'] = size;
+    styleSize['width.px'] = size;
+  }
+
+  private setStringStyleSize(styleSize: DataInfo<number | string>, size: string): void {
+    styleSize['height'] = size;
+    styleSize['width'] = size;
+  }
+
+  private setObjectStyleSize(
+    styleSize: DataInfo<number | string>,
+    size: {
+      height?: number | string;
+      width?: number | string;
+      'max-height'?: number | string;
+      'max-width'?: number | string;
+      'min-height'?: number | string;
+      'min-width'?: number | string;
+    }
+  ): void {
+    const { height, width, 'max-height': maxHeight, 'max-width': maxWidth, 'min-height': minHeight, 'min-width': minWidth } = size;
+
+    this.setDimensionStyleSize(styleSize, 'height', height);
+    this.setDimensionStyleSize(styleSize, 'width', width);
+    this.setDimensionStyleSize(styleSize, 'max-height', maxHeight);
+    this.setDimensionStyleSize(styleSize, 'max-width', maxWidth);
+    this.setDimensionStyleSize(styleSize, 'min-height', minHeight);
+    this.setDimensionStyleSize(styleSize, 'min-width', minWidth);
+  }
+
+  private setDimensionStyleSize(styleSize: DataInfo<number | string>, property: string, value?: number | string): void {
+    if (typeof value === 'string') {
+      styleSize[property] = value;
+    } else if (typeof value === 'number') {
+      styleSize[`${property}.px`] = value;
+    }
   }
 }
