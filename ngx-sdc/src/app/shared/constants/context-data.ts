2f7234b6e151202c82aab52489bd119f533446e5
Shagui <shaguixyz@gmail.com>
2024-06-26 13:36:44 +0200

    remove retrieve data

--- ngx-sdc/src/app/shared/constants/context-data.ts
+++ ngx-sdc/src/app/shared/constants/context-data.ts
@@ -1,5 +1,3 @@
-import { StorageService } from '@shagui/ng-shagui/core';
-
 export const SCHEDULER_PERIOD = 1 * 60 * 1000; // Minutes
 
 export enum ContextDataInfo {
@@ -11,15 +9,3 @@ export enum ContextDataInfo {
   OVERLAY_DATA = 'OVERLAY_DATA',
   SQUADS_DATA = 'SQUADS_DATA'
 }
-
-export const storageAppContextData = (storageService: StorageService) => {
-  storageService.create(ContextDataInfo.APPLICATIONS_DATA);
-  storageService.create(ContextDataInfo.DEPARTMENTS_DATA);
-  storageService.create(ContextDataInfo.SQUADS_DATA);
-};
-
-export const retrieveAppContextData = (storageService: StorageService) => {
-  storageService.retrieve(ContextDataInfo.APPLICATIONS_DATA);
-  storageService.retrieve(ContextDataInfo.DEPARTMENTS_DATA);
-  storageService.retrieve(ContextDataInfo.SQUADS_DATA);
-};
