6d6240bcecf7e8a59c43d3ef9ecb6d3a38dc8109
Shagui <shaguixyz@gmail.com>
2025-08-14 10:33:37 +0200

    :arrow_up: update translate loader version

--- ngx-sdc/src/app/shared/config/translate-utils.ts
+++ ngx-sdc/src/app/shared/config/translate-utils.ts
@@ -1,20 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { MissingTranslationHandler, MissingTranslationHandlerParams, TranslateLoader, TranslateModuleConfig } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MissingTranslationHandler, MissingTranslationHandlerParams, TranslateModuleConfig } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
-export const createTranslateLoader = (http: HttpClient) => new TranslateHttpLoader(http, 'assets/i18n/', '.json');
-
-export class CustomMissingTranslationHandler implements MissingTranslationHandler {
+class CustomMissingTranslationHandler implements MissingTranslationHandler {
   public handle(params: MissingTranslationHandlerParams): string {
     return `!${params.key}!`;
   }
 }
 
 export const TRANSLATE_MODULE_CONFIG: TranslateModuleConfig = {
-  loader: {
-    provide: TranslateLoader,
-    useFactory: createTranslateLoader,
-    deps: [HttpClient]
-  },
+  loader: provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' }),
   missingTranslationHandler: { provide: MissingTranslationHandler, useClass: CustomMissingTranslationHandler }
 };
